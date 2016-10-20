ea70ee9c357fcfa79193f2e7f585ed121757e803
Leonardo Dutra <leodutra.br@gmail.com>
2016-10-20 19:33:40 -0200

    Create spotify.sh

---
+++ environment/ubuntu-14-04/repository-setup/spotify.sh
@@ -0,0 +1,3 @@
+#!/bin/sh
+apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv-keys BBEBDCB318AD50EC6865090613B00F1FD2C19886
+echo deb http://repository.spotify.com stable non-free | tee /etc/apt/sources.list.d/spotify.list
